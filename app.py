b2e59413cf95b1ffa6ff501f0697e74f04ff3cf6
AfroLogicInsect <danielamahtoday@gmail.com>
2022-08-20 05:10:02 +0100

    Added Selfie

--- app.py
+++ app.py
@@ -4,14 +4,21 @@ import numpy as np
 import cv2
 from PIL import Image
 import io
+import os
 
 ## INFRASTRUCTURE
 mp_drawing = mp.solutions.drawing_utils
 mp_holistic = mp.solutions.holistic
 mp_pose = mp.solutions.pose
 
+## DEPENDENCIES AND DIRs
 DEMO_IMAGE = './content/sample.jpg'
 SHOWCASE_IMAGE = './content/showcase.jpg'
+
+def save_uploadedfile(uploadedfile):
+    with open(os.path.join("./content/", "selfie.jpg"), "wb") as f:
+        f.write(uploadedfile.getbuffer())
+
 
 st.title("Pose Estimation with MediaPipe")
 
@@ -61,7 +68,7 @@ def image_resize(image, width=None, height=None, inter=cv2.INTER_AREA):
 
 ## Various Application States
 app_mode = st.sidebar.selectbox('Choose the App Mode',
-                                ['About App', 'Run on Image', 'Run on Video']
+                                ['About App', 'Strike a Pose!', 'Run on Image']
                                 )
 
 if app_mode == 'About App':
@@ -88,12 +95,37 @@ if app_mode == 'About App':
 
     st.image(SHOWCASE_IMAGE)
 
-elif app_mode == 'Run on Image':
+if app_mode == "Strike a Pose!":
+    picture = st.camera_input("Take a picture")
+
+    if picture:
+        st.sidebar.image(picture, caption="Selfie")
+        if st.sidebar.button("Save Image"):
+            ## Function to save image
+            save_uploadedfile(picture)
+            st.sidebar.success("Saved File - Click to Download")
+            st.sidebar.write("Run on this Image in the next tab")
+            st.sidebar.markdown("---")
+            selfie_img = "./content/selfie.jpg"
+            with open(selfie_img, "rb") as file:
+                btn = st.sidebar.download_button(
+                    label="Download",
+                    data=file,
+                    file_name="selfie.jpg",
+                    mime="image/jpeg")
+
+    st.write("Click on **Clear photo** to retake picture")
+
+
+if app_mode == 'Run on Image':
     st.write("Please enter coloured images (for now)")
 
     img_file_buffer = st.sidebar.file_uploader("Upload an Image", type=["jpg", "jpeg", "png"])
     if img_file_buffer is not None:
         image = np.array(Image.open(img_file_buffer))
+    elif st.sidebar.button("Use Selfie"):
+        self_image = np.array(Image.open('./content/selfie.jpg'))
+        image = self_image
     else:  # default to demo image
         demo_image = DEMO_IMAGE
         image = np.array(Image.open(demo_image))
